31c6ef8b317abc369dbc15114f2a8403a2c8b73a
Karthikeyan S <karthikeyanssvk@gmail.com>
2022-04-24 17:06:32 +0530

    edit webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,38 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, './src/index.js'),
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [ 'babel-loader' ]
-            }
-        ]
-    },
-    resolve: {
-        extensions: [ '*', '.js', '.jsx' ],
-        alias: {
-            react: path.resolve('./node_modules/react'),
-            'react-dom': path.resolve('./node_modules/react-dom')
-        }
-    },
-    output: {
-        filename: 'index.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './index.html'),
-            inject: 'body'
-        })
+  mode: "development",
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
     ],
-    devServer: {
-        static: './',
-        port: 5001
-    }
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    alias: {
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+    },
+  },
+  output: {
+    filename: "index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./index.html"),
+      inject: "body",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 5001,
+  },
 };
